b279d6042f4a09109b87f6150aac7eda9ff7c1d6
januar <mail@januar.rhbirudhtugrethgrogiurtehguighe>
2025-11-25 00:05:54 +0100

    d0fd6001-f192-485a-b443-c37164830bfe

--- benchmark_merged.c
+++ benchmark_merged.c
@@ -5,12 +5,6 @@
 
 #define N 88888888  // Anzahl der Paare
 
-/*
- *0.003141
- *0.004787
- *0.002654
- * */
-
 // Variante 1: struct
 typedef struct {
     uint64_t nums[N];
@@ -70,6 +64,57 @@ int main(void) {
     }
     end = clock();
     printf("%f\n", (double)(end - start) / CLOCKS_PER_SEC);
+    free(mem);
+
+    // --- Variante 4: List of strings and numbers
+    void *mem = malloc((N * sizeof(uint64_t)) + (N * sizeof(char*)));
+    if (!mem) {
+        perror("malloc failed");
+        return 1;
+    }
+    uint64_t *nums = (uint64_t*)mem;
+    char **strs = (char**)(nums + N);
+    for (int i = 0; i < N; i++) {
+        nums[i] = (uint64_t)(rand() & 0xFFFFFFFF);
+        char buf[32];
+        snprintf(buf, sizeof(buf), "val_%u", rand() & 0xFFFF);
+        strs[i] = strdup(buf);
+    }
+    clock_t start = clock();
+    uint64_t sum = 0;
+    for (int i = 0; i < N; i++) {
+        sum += nums[i] + strlen(strs[i]);
+    }
+    clock_t end = clock();
+    printf("%f\n", (double)(end - start) / CLOCKS_PER_SEC);
+    for (int i = 0; i < N; i++) {
+        free(strs[i]);
+    }
+    free(mem);
+
+    //  --- Variante 5: List of strings and pointers
+    void *mem = malloc((N * sizeof(uint64_t)) + (N * sizeof(uint32_t*)));
+    if (!mem) {
+        perror("malloc failed");
+        return 1;
+    }
+    uint64_t *nums = (uint64_t*)mem;
+    uint32_t **strs = (uint32_t**)(nums + N);
+    for (int i = 0; i < N; i++) {
+        nums[i] = (uint64_t)(rand() & 0xFFFFFFFF);
+        strs[i] = malloc(sizeof(uint32_t));
+        *strs[i] = (uint32_t)(rand() & 0xFFFFFFFF);
+    }
+    clock_t start = clock();
+    uint64_t sum = 0;
+    for (int i = 0; i < N; i++) {
+        sum += nums[i] + *strs[i];
+    }
+    clock_t end = clock();
+    printf("%f\n", (double)(end - start) / CLOCKS_PER_SEC);
+    for (int i = 0; i < N; i++) {
+        free(strs[i]);
+    }
     free(mem);
     return 0;
 }
